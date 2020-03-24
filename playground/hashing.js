58439bc7b553333dda881622a3f96aee3f7a96ba
Farzan <farzanpira3@gmail.com>
2020-03-24 13:07:23 +0530

    added POST /users/login route

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,16 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
-var password = '123abc!';
+var password = '123abc';
 
-bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, (err, res) => {
-        console.log(res);
-    });
-});
-
-// var hashedPass = '$2a$10$wCTG0ZHttmNuI0dOkF6bIO6E7GURnnJeVPurqVesrB5m9XLkbVmNW';
-
-// bcrypt.compare(password, hashedPass, (err, res) => {
-//     console.log(res);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, res) => {
+//         console.log(res);
+//     });
 // });
+
+var hashedPass = '$2a$10$fJXXeDgkYH3baQvGSTVequ/9JfmaqeDhwN6KvwS9yNr/QBLGSW.6a';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+    console.log(res);
+});
--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,29 @@ UserSchema.methods.generateAuthToken = function() {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    console.log(err);
+                    console.log('crendials not found');
+                    reject(err);
+                }
+            });
+        });
+        
+    });
+}
+
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
@@ -59,11 +82,7 @@ UserSchema.statics.findByToken = function(token) {
     try{
         decoded = jwt.verify(token, 'abc123');
     } catch(e) {
-        // console.log(token);
-        // console.log('findByToken FAILED');
-        // console.log(e);
         return Promise.reject();
-        // return console.log(e);
     }
 
     return User.findOne({
